975025bba91c25283c38f313ad18047b2c1dcd7d
ike <ikewang@users.noreply.github.com>
2019-03-07 13:20:56 +0800

    Update HomeController.java
    111

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -1,7 +1,7 @@
 package com.gazgeek.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping(value = "/api/dataFactory")
 public class HomeController {
