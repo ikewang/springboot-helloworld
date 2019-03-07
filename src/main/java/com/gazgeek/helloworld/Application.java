31106334f612ad8a9ab37924b7620719550d4266
ike <ikewang@users.noreply.github.com>
2019-03-07 15:46:49 +0800

    Update Application.java
    gengxin le  集成

--- src/main/java/com/gazgeek/helloworld/Application.java
+++ src/main/java/com/gazgeek/helloworld/Application.java
@@ -2,11 +2,12 @@ package com.gazgeek.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) throws Exception {
